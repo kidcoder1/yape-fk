392a1deb24450ea3cae79fe784b13fa77956f83d
kidcoder1 <robert_22dr@hotmail.com>
2023-06-07 18:58:07 -0500

    cargar datos

---
+++ yape-fake/sweetAlert.js
@@ -0,0 +1,5 @@
+Swal.fire({
+  title: '¡Estos Chuchines!',
+  html: 'Esto es  <b>solo para bromas y juegos.</b><br>El que hace mal uso es un <b>ROSQUETE</b>, Seamos buenos, no hagen el mal y ya todos sabemos que ustedes son un poco chivatos pero que de ahi no pase.<br> Creador: <a class="beastam" href="https://t.me/miamorcindy">@MiAmorCindy</a>',
+
+})
